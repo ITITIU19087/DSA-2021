8338a358bcebd04f419cefb615916406e9c82892
ITITIU19087 <69331679+ITITIU19087@users.noreply.github.com>
2021-03-23 14:30:49 +0700

    Add files via upload

---
+++ StraightInsertionSort.java
@@ -0,0 +1,20 @@
+package sorting;
+import java.util.Comparator;
+public class StraightInsertionSort<E> implements ISort<E>{
+@Override
+    public void sort(E[] array, Comparator<E> comparator){
+        int current, walker;
+        E temp;
+        current = 1;
+        while(current < array.length){
+            temp = array[current];
+            walker = current - 1;
+            while((walker >= 0) && comparator.compare(temp, array[walker])< 0 ){
+                array[walker + 1] = array[walker]; //shift to right
+                walker -= 1;
+            }
+            array[walker + 1] = temp;
+            current += 1;
+        }
+    }
+}
